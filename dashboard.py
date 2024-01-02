2709d1f152aff45ab01be78bc8f987b2e6cadd29
Alex Haslam <alexhaslam12@gmail.com>
2024-01-02 18:44:43 +0000

    Set favicon in page_config

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ from training.model import VAE
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-st.set_page_config(layout="wide")
+st.set_page_config("Hand-writing generator", page_icon="writing_hand", layout="wide")
 
 
 @st.cache_resource()
